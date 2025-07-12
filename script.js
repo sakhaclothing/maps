77d05dbf4792b2ddb9ce6e422e6fc487bf80737f
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2025-07-12 12:18:53 +0700

    ok

--- script.js
+++ script.js
@@ -86,14 +86,13 @@ function showFallbackMap() {
 // Create custom marker with animation
 function createCustomMarker() {
     try {
-        const markerIcon = {
+        const customMarkerIcon = {
             url: 'data:image/svg+xml;charset=UTF-8,' + encodeURIComponent(`
-            <svg width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="20" cy="20" r="18" fill="#000000" stroke="#fff" stroke-width="2"/>
-                <circle cx="20" cy="20" r="8" fill="#fff"/>
-                <circle cx="20" cy="20" r="4" fill="#000000"/>
-            </svg>
-        `),
+                <svg width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="20" cy="20" r="18" fill="black" stroke="white" stroke-width="2"/>
+                    <circle cx="20" cy="20" r="8" fill="white"/>
+                </svg>
+            `),
             scaledSize: new google.maps.Size(40, 40),
             anchor: new google.maps.Point(20, 20)
         };
@@ -101,7 +100,7 @@ function createCustomMarker() {
         marker = new google.maps.Marker({
             position: SAKHA_LOCATION,
             map: map,
-            icon: markerIcon,
+            icon: customMarkerIcon,
             title: 'Sakha Clothing',
             animation: google.maps.Animation.DROP
         });
